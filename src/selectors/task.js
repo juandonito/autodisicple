ffea09c3b9c676637cf7a82080b130eb02b46dea
juandonito <anguadir@gmail.com>
2019-05-31 19:36:17 +0200

    Works locally

--- src/selectors/task.js
+++ src/selectors/task.js
@@ -1,4 +1,8 @@
-const sameDay = (dateOne, dateTwo) => {
+const sameDay = (dOne, dTwo) => {
+    //makes sure that we have the same date format
+    const dateOne = new Date(dOne);
+    const dateTwo = new Date(dTwo);
+    
     return dateOne.getFullYear() === dateTwo.getFullYear() &&
         dateOne.getMonth() === dateTwo.getMonth() &&
         dateOne.getDate() === dateTwo.getDate();
@@ -10,5 +14,5 @@ export const getTodayTask = (state) => {
 }
 
 export const getNotDoneTask = (state) => {
-    return state.tasksState.filter(task => task.done == false);
+    return state.tasksState.filter(task => task.done === false);
 }
--- src/store/index.js
+++ src/store/index.js
@@ -2,14 +2,26 @@ import { createStore, applyMiddleware } from 'redux'
 import { createLogger } from 'redux-logger'
 import rootReducer from '../reducers'
 
+import { loadState, saveState } from '../localStorage'
+
 const logger = createLogger();
+
+const persistedState = loadState();
+
+console.log(persistedState);
 
 const store = createStore(
     rootReducer,
-    undefined,
+    persistedState,
     applyMiddleware(
         logger
     )
 )
+
+store.subscribe(() => {
+    saveState({
+        ...store.getState()
+    });
+});
 
 export default store;
